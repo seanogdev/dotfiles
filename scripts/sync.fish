296345b490b0d7bef2701eb96ea57a8a0d9881b5
Sean O'Grady <sean@seanogrady.me>
2021-02-22 00:09:26 +0000

    fix paths

--- scripts/sync.fish
+++ scripts/sync.fish
@@ -1,26 +1,23 @@
-set -l DIR (dirname "$0")
-cd $DIR
-
-ln -sf $HOME/dotfiles/fish $HOME/.config
-ln -sf $HOME/dotfiles/.gitconfig $HOME/
-ln -sf $HOME/dotfiles/.editorconfig $HOME/
-ln -sf $HOME/dotfiles/sync/.Brewfile $HOME/
-ln -sf $HOME/dotfiles/sync/.Npmfile $HOME/
+ln -sf $DOTFILES_PATH/fish $HOME/.config
+ln -sf $DOTFILES_PATH/.gitconfig $HOME/
+ln -sf $DOTFILES_PATH/.editorconfig $HOME/
+ln -sf $DOTFILES_PATH/sync/.Brewfile $HOME/
+ln -sf $DOTFILES_PATH/sync/.Npmfile $HOME/
 
 # sync sensitive functions
-set -l cloudFunctionsPath "$ICLOUD_DOTFILES_PATH/fish/functions"
+set -l cloudFunctionsPath $ICLOUD_DOTFILES_PATH/fish/functions
 if test -d $cloudFunctionsPath
-    ln -sf $cloudFunctionsPath/* $HOME/dotfiles/fish/functions
+    ln -sf $cloudFunctionsPath/* $DOTFILES_PATH/fish/functions
 end
 
 # sync fonts
-set -l cloudFontsPath "$ICLOUD_DOTFILES_PATH/sync/fonts"
+set -l cloudFontsPath $ICLOUD_DOTFILES_PATH/sync/fonts
 if test -d $cloudFontsPath
-    ln -sf $cloudFontsPath $HOME/Library/Fonts/CloudFonts
+    ln -sf $cloudFontsPath $HOME/Library/Fonts/
 end
 
 # sync ngrok
-set -l cloudNgrokPath "$ICLOUD_DOTFILES_PATH/sync/ngrok.yml"
+set -l cloudNgrokPath $ICLOUD_DOTFILES_PATH/sync/ngrok.yml
 if test -e $cloudNgrokPath
     ln -sf $cloudNgrokPath $HOME/ngrok.yml
 end
