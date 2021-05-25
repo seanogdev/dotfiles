99efb355d315dfc3208ee5ca09f5a7e0f956696f
Sean O'Grady <sean@seanogrady.me>
2021-05-25 10:07:54 +0100

    fnm changes

--- fish/conf.d/fnm.fish
+++ fish/conf.d/fnm.fish
@@ -1,9 +1,9 @@
-set -gx PATH /var/folders/8d/w_mvjyy91v7dwvtllyzljrz40000gn/T/fnm_multishells/2021_1618965285286/bin $PATH
-set -gx FNM_MULTISHELL_PATH /var/folders/8d/w_mvjyy91v7dwvtllyzljrz40000gn/T/fnm_multishells/2021_1618965285286
-set -gx FNM_DIR "/Users/seanogrady/.fnm"
-set -gx FNM_LOGLEVEL info
-set -gx FNM_NODE_DIST_MIRROR "https://nodejs.org/dist"
-set -gx FNM_ARCH x64
+set -gx PATH "/var/folders/8d/w_mvjyy91v7dwvtllyzljrz40000gn/T/fnm_multishells/2940_1621737584677/bin" $PATH;
+set -gx FNM_MULTISHELL_PATH "/var/folders/8d/w_mvjyy91v7dwvtllyzljrz40000gn/T/fnm_multishells/2940_1621737584677";
+set -gx FNM_DIR "/Users/seanogrady/.fnm";
+set -gx FNM_LOGLEVEL "info";
+set -gx FNM_NODE_DIST_MIRROR "https://nodejs.org/dist";
+set -gx FNM_ARCH "x64";
 function _fnm_autoload_hook --on-variable PWD --description 'Change Node version on directory change'
     status --is-command-substitution; and return
     if test -f .node-version -o -f .nvmrc
@@ -12,9 +12,3 @@ function _fnm_autoload_hook --on-variable PWD --description 'Change Node version
 end
 
 _fnm_autoload_hook
-
-fnm env | source
-
-if test -f .node-version -o -f .nvmrc
-    fnm use
-end
