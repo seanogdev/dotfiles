9bb937f3c77d3f0f0140da107ca39797d68e5f67
Sean O'Grady <1761115+miralize@users.noreply.github.com>
2022-05-18 03:28:55 +0100

    updates

--- fish/functions/mkd.fish
+++ fish/functions/mkd.fish
@@ -1,9 +1,13 @@
 function mkd --description 'Create a new directory and navigate to it'
     if [ -z "$argv" ]
         echo "No arguments supplied"
-        return
     else
-        mkdir -p $argv
-        cd $argv
+        if [ -d "$argv" ]
+            echo "Directory already exists, cd'ing into it"
+            cd "$argv"
+        else
+            mkdir "$argv"
+            cd "$argv"
+        end
     end
 end
