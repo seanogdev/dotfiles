d5e1b80c2742b6ae7dc074fb5e74d32763152a12
Sean O'Grady <1761115+seanogdev@users.noreply.github.com>
2022-10-19 00:08:47 +0100

    colima

--- fish/conf.d/dockerhost.fish
+++ fish/conf.d/dockerhost.fish
@@ -1,4 +1,5 @@
 if type -f docker
-    set -gx DOCKER_HOST (docker context inspect --format '{{.Endpoints.docker.Host}}')
+    docker context use colima;
+    set -gx DOCKER_HOST (docker context inspect --format '{{.Endpoints.docker.Host}}');
 end
 
