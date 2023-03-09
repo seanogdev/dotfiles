b81d1c767293b9a6f3791a10fd8d70b8bf26a42d
Sean O'Grady <1761115+seanogdev@users.noreply.github.com>
2023-03-09 22:36:58 +0000

    new dots

---
+++ brew/install-brew.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+info "Installing Homebrew..."
+if [[ $(command -v brew) == "" ]]; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
