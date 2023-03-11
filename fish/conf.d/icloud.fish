2a325a75de00c2f3f21cf0972ebdf242b89728df
Sean O'Grady <1761115+seanogdev@users.noreply.github.com>
2023-03-11 01:07:11 +0000

    Font sync back

--- fish/conf.d/icloud.fish
+++ fish/conf.d/icloud.fish
@@ -1,2 +1,11 @@
-set -x DOTFILES_PATH $HOME/dotfiles
 set -x ICLOUD_DOTFILES_PATH "$HOME/Library/Mobile Documents/com~apple~CloudDocs/Code/dotfiles"
+
+## fonts
+if test -d "$ICLOUD_DOTFILES_PATH/sync/fonts"
+    cp -Rf "$ICLOUD_DOTFILES_PATH/sync/fonts/"* "$HOME/Library/Fonts/"
+end
+
+## sensitive functions
+if test -d "$ICLOUD_DOTFILES_PATH/sync/fish/functions"
+    ln -sf  "$ICLOUD_DOTFILES_PATH/sync/fish/functions"* "$HOME/.config/fish/functions/"
+end
