289119a5785c7d4da8aadf64a4911628e71a7b68
Sean O'Grady <1761115+seanogdev@users.noreply.github.com>
2023-03-11 01:07:11 +0000

    Font sync back

--- install/setup.sh
+++ install/setup.sh
@@ -1,12 +1,35 @@
-#! /usr/bin/env sh
+#! /usr/bin/env bash
 
-DIR=$(dirname "$0")
-cd "$DIR"
+cd "$(dirname "$0")/.."
+DOTFILES=$(pwd -P)
 
-sudo -v
+set -e
+
+echo ''
+
+info () {
+  printf "\r  [ \033[00;34m..\033[0m ] %s\n" "$1"
+}
+
+user () {
+  printf "\r  [ \033[0;33m??\033[0m ] %s\n" "$1"
+}
+
+success () {
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] %s" "$1"
+}
+
+fail () {
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] %s\n" "$1"
+  echo ''
+  exit
+}
 
 info "Installing Homebrew..."
-if [[ $(command -v brew) == "" ]]; then
+
+# if brew is not installed
+if ! command -v brew &> /dev/null
+then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
     brew update
@@ -20,13 +43,13 @@ info "Changing default shell"
 
 FISH_PATH="/usr/local/bin/fish"
 
-if [[ `uname -m` == 'arm64' ]]; then
+if [[ $(uname -m) == 'arm64' ]]; then
     FISH_PATH="/opt/homebrew/bin/fish"
 fi
 
 echo $FISH_PATH | sudo tee -a /etc/shells
 
-chsh -s `which fish`
+chsh -s "$(which fish)"
 
 sh ./symlink.sh
 
