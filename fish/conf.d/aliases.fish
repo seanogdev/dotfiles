8bea0dc76f6b007aa977cb7ef7d1c973e30b635a
Sean O'Grady <1761115+seanogdev@users.noreply.github.com>
2023-03-11 02:31:05 +0000

    fzf

--- fish/conf.d/aliases.fish
+++ fish/conf.d/aliases.fish
@@ -1,36 +1,19 @@
 #! /usr/bin/env fish
 
-# Easier navigation: .., ..., ...., ....., ~ and -
-function ..
-    cd ..
-end
-function ...
-    cd ../..
-end
-function ....
-    cd ../../..
-end
-function .....
-    cd ../../../..
-end
+# Easier navigation: .., ..., ...., .....
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
 
-# Google Chrome
-alias chrome="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
-alias canary="/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary"
+# Shortcuts
+alias ls=exa
+alias sl=exa
+alias c='clear'
 
 # IP addresses
 alias ip-wan="dig +short myip.opendns.com @resolver1.opendns.com"
 alias ip-lan="ipconfig getifaddr en0"
 alias ip-all="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
-
-# Show active network interfaces
-alias ifactive="ifconfig | pcregrep -M -o '^[^\t:]+:([^\n]|\n\t)*status: active'"
-
-# Flush Directory Service cache
-alias flush="dscacheutil -flushcache; killall -HUP mDNSResponder"
-
-# Clean up LaunchServices to remove duplicates in the “Open With” menu
-alias lscleanup="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user; killall Finder"
 
 # Utility commands
 alias s="source ~/.config/fish/config.fish"
