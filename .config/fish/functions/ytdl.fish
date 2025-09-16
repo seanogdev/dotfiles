c7dafa350e4cee34c7d14da311f21abcc2420bc8
Sean O'Grady <1761115+seanogdev@users.noreply.github.com>
2025-09-16 20:04:02 +0100

    fix: update function descriptions and replace youtube-dl with yt-dlp

--- .config/fish/functions/ytdl.fish
+++ .config/fish/functions/ytdl.fish
@@ -1,7 +1,7 @@
 function ytdl --description 'Download best quality youtube video'
-    youtube-dl -i -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]" --merge-output-format mp4 -o "~/Downloads" $argv
+    yt-dlp -i -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]" --merge-output-format mp4 -o "~/Downloads" $argv
 end
 
 function ytdl-audio --description 'Download best quality youtube video (audio-only)'
-    youtube-dl -i -f "bestaudio[ext=m4a]" -o "~/Downloads" $argv
+    yt-dlp -i -f "bestaudio[ext=m4a]" -o "~/Downloads" $argv
 end
