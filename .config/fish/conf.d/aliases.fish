2abcac2eb331ca73803310fa4adf33d6804dbfae
Sean O'Grady <1761115+seanogdev@users.noreply.github.com>
2025-09-16 20:01:49 +0100

    fix: Remove unused utility command alias

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -13,6 +13,4 @@ alias ip-wan="dig +short myip.opendns.com @resolver1.opendns.com"
 alias ip-lan="ipconfig getifaddr en0"
 alias ip-all="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
 
-# Utility commands
-alias s="source ~/.config/fish/config.fish"
 
