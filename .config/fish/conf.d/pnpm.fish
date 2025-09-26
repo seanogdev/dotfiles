1056890a74501e7c6677005ccac9a4970923d080
Sean O'Grady <1761115+seanogdev@users.noreply.github.com>
2025-09-26 23:11:35 +0100

    fix: remove empty-trash function and clean up starship configuration

--- .config/fish/conf.d/pnpm.fish
+++ .config/fish/conf.d/pnpm.fish
@@ -1,3 +1,2 @@
 set -gx PNPM_HOME "$HOME/Library/pnpm"
 set -gx PATH "$PNPM_HOME" $PATH
-
--- .config/fish/functions/empty-trash.fish
+++
@@ -1,5 +0,0 @@
-function emptytrash
-    for files in ~/.Trash/
-        sudo rm -rdfv $files
-    end
-end
