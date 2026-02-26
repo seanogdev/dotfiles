30a10ed00fe6021437b4fda68608db7eecea24d0
Sean O'Grady <1761115+seanogdev@users.noreply.github.com>
2026-02-26 02:16:52 +0000

    feat: clean up orphaned symlinks after stowing dotfiles
    After stow runs, find and remove any broken symlinks in $HOME to keep
    the home directory free of dangling links left by removed dotfiles.

--- .config/fish/functions/stow-local.fish
+++ .config/fish/functions/stow-local.fish
@@ -1,6 +1,14 @@
 function stow-local --description "Stow dotfiles in the local directory"
     if test -d $DOTFILES_DIR
         stow -d $DOTFILES_DIR -t $HOME --no-folding --adopt --stow .
+        set broken (find $HOME -xtype l 2>/dev/null)
+        if test (count $broken) -gt 0
+            echo "Removing orphaned symlinks:"
+            for link in $broken
+                echo "  $link"
+            end
+            find $HOME -xtype l -delete 2>/dev/null
+        end
     else
         echo "Local dotfiles directory does not exist."
     end
