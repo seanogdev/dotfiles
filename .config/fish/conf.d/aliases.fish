f47e851b4e3937c194ee3473f2a98688996a40e0
Sean O'Grady <1761115+seanogdev@users.noreply.github.com>
2026-02-28 21:52:19 +0000

    feat: update .Brewfile and CLAUDE.md with new dependencies and improved guidelines; remove karpathy-guidelines

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -5,12 +5,3 @@ alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
 
-# Shortcuts
-alias rm=trash
-
-# IP addresses
-alias ip-wan="dig +short myip.opendns.com @resolver1.opendns.com"
-alias ip-lan="ipconfig getifaddr en0"
-alias ip-all="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
-
-
