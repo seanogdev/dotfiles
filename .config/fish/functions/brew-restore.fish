8db8b4bd1bdf9259d1c00a1746bbd2bfa630d366
Sean O'Grady <1761115+seanogdev@users.noreply.github.com>
2026-09-01 12:49:11 +0100

    fix(brew-restore): clean up before installing

--- .config/fish/functions/brew-restore.fish
+++ .config/fish/functions/brew-restore.fish
@@ -1,4 +1,4 @@
 function brew-restore --description "Sync installed brews to match $HOME/.Brewfile"
-    brew bundle install --global
     brew bundle cleanup --global --force --zap
+    brew bundle install --global
 end
